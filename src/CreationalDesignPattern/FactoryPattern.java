eceec5032d5510b82d76d28d13e8c2a5a83ffbff
wasir <live.wasir@gmail.com>
2018-09-03 23:17:54 +0600

    add folders

--- src/CreationalDesignPattern/FactoryPattern.java
+++ src/CreationalDesignPattern/FactoryPattern.java
@@ -3,19 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package designpatterns;
+package CreationalDesignPattern;
 
 /**
  *
  * @author wasir
  */
-public class DesignPatterns {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class FactoryPattern {
     
 }
